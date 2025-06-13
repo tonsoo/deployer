f142d0b72c539a0642046a04af7846461e8ed3d1
Alysson <alyssuuuuu@outlook.com>
2025-06-12 22:09:39 -0300

    fix

--- app/deploy.sh
+++ app/deploy.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-echo "Running deploy for project $APP_DOCKER_NAME"
+echo "Running deploy for project $1"
 
 echo "Installing dependencies"
-docker compose -p "$APP_DOCKER_NAME" exec -T php composer install --no-dev
+docker compose -p "$1" exec -T php composer install --no-dev
 
 echo "Migrating database"
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan migrate --force
+docker compose -p "$1" exec -T php php artisan migrate --force
 
 echo "Cleaning cache"
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan config:clear
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan cache:clear
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan config:cache
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan view:clear
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan optimize:clear
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan optimize
+docker compose -p "$1" exec -T php php artisan config:clear
+docker compose -p "$1" exec -T php php artisan cache:clear
+docker compose -p "$1" exec -T php php artisan config:cache
+docker compose -p "$1" exec -T php php artisan view:clear
+docker compose -p "$1" exec -T php php artisan optimize:clear
+docker compose -p "$1" exec -T php php artisan optimize
 
 echo "Linking storage"
-docker compose -p "$APP_DOCKER_NAME" exec -T php php artisan storage:link
+docker compose -p "$1" exec -T php php artisan storage:link
